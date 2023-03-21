69822a2d13a875bbf8f79b1bc228f763c4e22c22
김지원 <jwk4945@gmail.com>
2023-03-21 17:17:03 +0900

    button까지 수정, 푸터 붙이는 중

---
+++ resources/v1/js/link.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    // 교보문고 메인 - 홈
+    $('#link_home').attr('href', 'https://www.kyobobook.co.kr/');
+    // 교보문고 메인
+    $('#link_kyoboBook').attr('href', 'https://www.kyobobook.co.kr/');
+    // 사업자정보확인
+    $('#link_companyChk').attr('href', 'https://www.ftc.go.kr/bizCommPop.do?wrkr_no=1028111670&apv_perm_no=');
+    // 구매인전서비스
+    $('#link_paychk').attr('href', 'https://pgweb.uplus.co.kr/ms/escrow/s_escrowYn.do?mertid=ink0911');
+    // 로그인
+    $('#link_login').attr('href', 'https://mmbr.kyobobook.co.kr/login');
+    // 회원가입
+    $('#link_join').attr('href', 'https://mmbr.kyobobook.co.kr/join');
+    // 이용약관
+    $('#link_provision').attr('href', 'https://www.kyobobook.co.kr/contents/provision');
+    // 개인정보처리방침
+    $('#link_privacy').attr('href', 'https://www.kyobobook.co.kr/contents/privacy-policy');
+    // 청소년보호정책
+    $('#link-youth').attr('href', 'https://www.kyobobook.co.kr/contents/youth-policy');
+    //협력사이러분
+    $('#link_chargeperson').attr('href', 'https://www.kyobobook.co.kr/partners/chargeperson');
+});
