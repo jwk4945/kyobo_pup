cdd6aa5018fe05c9ee58d77af1045d19ccb42510
김지원 <jwk4945@gmail.com>
2023-04-20 09:02:51 +0900

    인덱스 정리, pc화면

---
+++ resources/v1/js/link_pc.js
@@ -0,0 +1,20 @@
+$(document).ready(function (){
+    // 인터넷 교보문고
+    $('#link_home').attr('href', 'http://www.kyobobook.co.kr/index.laf');
+    //매장안내
+    $('#link_store').attr('href', 'http://www.kyobobook.co.kr/storen/MainStore.laf?SITE=01');
+    // 사업자정보확인
+    // $('#link_companyChk').attr('href', 'https://www.ftc.go.kr/bizCommPop.do?wrkr_no=1028111670&apv_perm_no=');
+    // 구매인전서비스
+    // $('#link_paychk').attr('href', 'https://pgweb.uplus.co.kr/ms/escrow/s_escrowYn.do?mertid=ink0911');
+    // 로그인
+    // $('#link_login').attr('href', 'https://mmbr.kyobobook.co.kr/login');
+    // 회원가입
+    // $('#link_join').attr('href', 'https://mmbr.kyobobook.co.kr/join');
+    // 이용약관
+    // $('#link_provision').attr('href', 'https://www.kyobobook.co.kr/contents/provision');
+    // 개인정보처리방침
+    // $('#link_privacy').attr('href', 'https://www.kyobobook.co.kr/contents/privacy-policy');
+    // 청소년보호정책
+    // $('#link_youth').attr('href', 'https://www.kyobobook.co.kr/contents/youth-policy');
+});
