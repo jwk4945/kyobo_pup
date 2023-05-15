48521ecd8e7031aa21b78f70f25a0cf420ec10bd
김지원 <jwk4945@gmail.com>
2023-05-15 15:34:32 +0900

    공유하기 팝업 붙였다~~

--- resources/v1/js/common.js
+++ resources/v1/js/common.js
@@ -1,4 +1,5 @@
 $(document).ready(function (){
+    // footer
     $('.footer_body .btn_more_body').on('click', function(){
         if($(this).hasClass('active')){
             $(this).removeClass('active');
@@ -8,4 +9,34 @@ $(document).ready(function (){
             $('.footer_body .footer_info_box').addClass('active');
         }
     });
+
+    // SNS 공유하기
+    $('#link_share').on('click', function(){
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+            $('#popShare').removeClass('active');
+            $('.dim').removeClass('active');
+            $('html').removeClass('lock');
+        } else{
+            $(this).addClass('active');
+            $('#popShare').addClass('active');
+            $('.dim').addClass('active');
+            $('html').addClass('lock');
+        }
+    });
+
+    $('#popShare').on('click', function(){
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+            $('#link_share').removeClass('active');
+            $('.dim').removeClass('active');
+            $('html').removeClass('lock');
+        } else{
+            $(this).addClass('active');
+            $('#link_share').addClass('active');
+            $('.dim').addClass('active');
+            $('html').addClass('lock');
+        }
+    });
+    // //SNS 공유하기
 });
