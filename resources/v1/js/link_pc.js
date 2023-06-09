69fc15a2385799821316f1210e34b5c0a0fbf16e
김지원 <jwk4945@gmail.com>
2023-06-09 13:31:49 +0900

    인덱스 pc리스트 추가

--- resources/v1/js/link_pc.js
+++ resources/v1/js/link_pc.js
@@ -3,18 +3,24 @@ $(document).ready(function (){
     $('#link_home').attr('href', 'http://www.kyobobook.co.kr/index.laf');
     //매장안내
     $('#link_store').attr('href', 'http://www.kyobobook.co.kr/storen/MainStore.laf?SITE=01');
+    // 로그인
+    $('#link_login').attr('href', 'https://mmbr.kyobobook.co.kr/login?continue=https%3A%2F%2Fwww.kyobobook.co.kr%2F');
+    // 회원가입
+    $('#link_join').attr('href', 'https://mmbr.kyobobook.co.kr/join');
+    // 이용약관
+    $('#link_provision').attr('href', 'http://www.kyobobook.co.kr/contents/provision.laf?orderClick=rXc');
+    // 개인정보처리방침
+    $('#link_privacy').attr('href', 'http://www.kyobobook.co.kr/contents/privacyPolicy.laf?orderClick=rXd');
+    // 청소년보호정책
+    $('#link_youth').attr('href', 'http://www.kyobobook.co.kr/contents/youthPolicy.laf');
+    // 제휴·제안
+    $('#link_suggest').attr('href', 'https://www.kyobobook.co.kr/guidePublisher/suggest.laf?orderClick=rXi');
+    // 고객센터
+    $('#link_csCenter').attr('href', 'http://www.kyobobook.co.kr/cscenter/csCenterMain.laf?Kc=GNHHNOcscenter&amp;orderClick=rXo');
+    // 매장안내
+    $('#link_store').attr('href', 'http://www.kyobobook.co.kr/storen/MainStore.laf?SITE=01');
     // 사업자정보확인
     // $('#link_companyChk').attr('href', 'https://www.ftc.go.kr/bizCommPop.do?wrkr_no=1028111670&apv_perm_no=');
     // 구매인전서비스
     // $('#link_paychk').attr('href', 'https://pgweb.uplus.co.kr/ms/escrow/s_escrowYn.do?mertid=ink0911');
-    // 로그인
-    // $('#link_login').attr('href', 'https://mmbr.kyobobook.co.kr/login');
-    // 회원가입
-    // $('#link_join').attr('href', 'https://mmbr.kyobobook.co.kr/join');
-    // 이용약관
-    // $('#link_provision').attr('href', 'https://www.kyobobook.co.kr/contents/provision');
-    // 개인정보처리방침
-    // $('#link_privacy').attr('href', 'https://www.kyobobook.co.kr/contents/privacy-policy');
-    // 청소년보호정책
-    // $('#link_youth').attr('href', 'https://www.kyobobook.co.kr/contents/youth-policy');
 });
