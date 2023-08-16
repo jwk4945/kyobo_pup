498b1d3ddcd388d62dbdc4f9b908bbe64bcd3497
suhny08 <suhny08@gmail.com>
2023-08-16 16:15:25 +0900

    콘텐츠 별 연결 상품 매칭

--- resources/v1/js/insData.js
+++ resources/v1/js/insData.js
@@ -1,18 +1,21 @@
 /*
-** 06/29 v2(확대오픈)
-1	 자기개발 	        16B_011
-2	 자녀_초중등 	       1B_067
-3	 질병_척추/근골격계   4B_043
-4	 일상생활_부부 	     8B_110
-5	 건강관리_운동 외 	  7D_003
-*
-** 08/31 o1(정식오픈)
-* 18B_023
-* 9B_062
-* 12G_108
-* 14G_057
-* 19G_142
-*/
+ * * 08/31 정식오픈 콘텐츠ID
+2B_061
+3C_033
+6G_054
+13G_045
+5B_041
+16B_011
+1B_067
+4B_043
+8B_110
+7D_003
+9B_062
+12G_108
+14G_057
+18B_023
+19G_142
+* */
 export default
 [
     {
@@ -42,215 +45,258 @@ export default
         }
     },
     {
-        contentsId: ["16B_011", "18B_023", "9B_062", "12G_108", "14G_057", "19G_142"],
+        contentsId: "2B_061",
         linkInfoForInsurance: {
-            url: "https://www.kyobo.com/dgt/web/dtp/insurance-detail?isPdtCd=1000938&evcode=DCJ_MO",
-            name: "교보1년저축보험(무배당)[D]",
-            imgUrl: "/resources/v1/img/img-ci-pd.png",
-            explain: "소액으로 차근차근 오늘부터 시작해요!",
-            certificationMsg: "생명보험협회 심의필 제 2023-02901호",
-            period: "(2023-07-04 ~ 2024-07-03)"
+            url: "https://www.kyobo.com/dgt/web/dtp/ei/insurance-detail/1000976&evcode=KB_CJ_EI",
+            name: "교보e1년저축보험(무배당)",
+            imgUrl: '/resources/v1/img/img-ci-pd.png',
+            explain: '부족한 보장을 꽉 채운 건강보험',
+            certificationMsg: '생명보험협회 심의필 제 2022-05466호',
+            period: "(2022-12-27 ~ 2023-12-26)"
         },
         gaParams: {
-            // 매개변수1 : [ep_button_area] 자기개발_요즘20대들은모른다는문제_상세
-            params1: "자기개발_요즘20대들은모른다는문제_상세",
-            // 매개변수2: [ep_button_area2] 상품페이지 이동
+            params1: "질병_척추근골격계_버섯목증후군_상세",
             params2: "상품페이지 이동",
-            // 매개변수3: [ep_button_name] 배너선택
             params3: "배너선택",
-            // 매개변수4: [ep_click_variable] "{{상품}}" 상품_(무)교보시작해요저축보험
-            params4: "상품_교보1년저축보험(무배당)[D]",
+            params4: "DBS_상품_(무)교보내맘쏙건강보험(DM)",
+        }
+    },
+    {
+        contentsId: "3C_033",
+        linkInfoForInsurance: {
+            url: "https://mdirect.kyobo.co.kr:8877/biz/intro/introMain.do?wrkType=GM_EV_PM",
+            name: "내보험바로알기 서비스",
+            imgUrl: '/resources/v1/img/img-ci-pd.png',
+            explain: '부족한 보장을 꽉 채운 건강보험',
+            certificationMsg: '생명보험협회 심의필 제 2022-05466호',
+            period: "(2022-12-27 ~ 2023-12-26)"
+        },
+        gaParams: {
+            params1: "질병_척추근골격계_버섯목증후군_상세",
+            params2: "상품페이지 이동",
+            params3: "배너선택",
+            params4: "DBS_상품_(무)교보내맘쏙건강보험(DM)",
+        }
+    },
+    {
+        contentsId: "6G_054",
+        linkInfoForInsurance: {
+            url: "https://mdirect.kyobo.co.kr:8877/biz/intro/introMain.do?wrkType=GM_EV_PM",
+            name: "내보험바로알기 서비스",
+            imgUrl: '/resources/v1/img/img-ci-pd.png',
+            explain: '부족한 보장을 꽉 채운 건강보험',
+            certificationMsg: '생명보험협회 심의필 제 2022-05466호',
+            period: "(2022-12-27 ~ 2023-12-26)"
+        },
+        gaParams: {
+            params1: "질병_척추근골격계_버섯목증후군_상세",
+            params2: "상품페이지 이동",
+            params3: "배너선택",
+            params4: "DBS_상품_(무)교보내맘쏙건강보험(DM)",
+        }
+    },
+    {
+        contentsId: "13G_045",
+        linkInfoForInsurance: {
+            url: "https://mdirect.kyobo.co.kr:8877/biz/intro/introMain.do?wrkType=GM_EV_PM",
+            name: "내보험바로알기 서비스",
+            imgUrl: '/resources/v1/img/img-ci-pd.png',
+            explain: '부족한 보장을 꽉 채운 건강보험',
+            certificationMsg: '생명보험협회 심의필 제 2022-05466호',
+            period: "(2022-12-27 ~ 2023-12-26)"
+        },
+        gaParams: {
+            params1: "질병_척추근골격계_버섯목증후군_상세",
+            params2: "상품페이지 이동",
+            params3: "배너선택",
+            params4: "DBS_상품_(무)교보내맘쏙건강보험(DM)",
+        }
+    },
+    {
+        contentsId: "5B_041",
+        linkInfoForInsurance: {
+            url: "https://www.kyobo.com/dgt/web/dtp/ei/insurance-detail/1000971&evcode=KB_CJ_EI",
+            name: "교보e상해케어보험(무배당)",
+            imgUrl: '/resources/v1/img/img-ci-pd.png',
+            explain: '부족한 보장을 꽉 채운 건강보험',
+            certificationMsg: '생명보험협회 심의필 제 2022-05466호',
+            period: "(2022-12-27 ~ 2023-12-26)"
+        },
+        gaParams: {
+            params1: "질병_척추근골격계_버섯목증후군_상세",
+            params2: "상품페이지 이동",
+            params3: "배너선택",
+            params4: "DBS_상품_(무)교보내맘쏙건강보험(DM)",
+        }
+    },
+    {
+        contentsId: "16B_011",
+        linkInfoForInsurance: {
+            url: "https://www.kyobo.com/dgt/web/dtp/ei/insurance-detail/1000976&evcode=KB_CJ_EI",
+            name: "교보e1년저축보험(무배당)",
+            imgUrl: '/resources/v1/img/img-ci-pd.png',
+            explain: '부족한 보장을 꽉 채운 건강보험',
+            certificationMsg: '생명보험협회 심의필 제 2022-05466호',
+            period: "(2022-12-27 ~ 2023-12-26)"
+        },
+        gaParams: {
+            params1: "질병_척추근골격계_버섯목증후군_상세",
+            params2: "상품페이지 이동",
+            params3: "배너선택",
+            params4: "DBS_상품_(무)교보내맘쏙건강보험(DM)",
         }
     },
     {
         contentsId: "1B_067",
         linkInfoForInsurance: {
-            url: "http://www.kyobo.com/dgt/web/dtp/insurance-detail?isPdtCd=1000948&evcode=DCJ_MO",
-            name: "(무)교보금쪽같은내아이보험(갱신형)Ⅱ(DM)",
-            imgUrl: "/resources/v1/img/img-ci-pd.png",
-            explain: "내 아이를 위한 금쪽같은 어린이 보험",
-            certificationMsg: "생명보험협회 심의필 제 2022-05464호",
-            period: '(2022-12-27 ~ 2023-12-26)'
-        },
-        gaParams: {
-            // 매개변수1 : [ep_button_area] 자기개발_요즘20대들은모른다는문제_상세
-            params1: "자녀_초중등_가스라이팅_상세",
-            // 매개변수2: [ep_button_area2] 상품페이지 이동
-            params2: "상품페이지 이동",
-            // 매개변수3: [ep_button_name] 배너선택
-            params3: "배너선택",
-            // 매개변수4: [ep_click_variable] "{{상품}}" 상품_(무)교보시작해요저축보험
-            params4: "상품_(무)교보금쪽같은내아이보험(갱신형)Ⅱ(DM)",
-        }
-    },
-    {
-        contentsId : "4B_043",
-        linkInfoForInsurance: {
-            url: 'http://www.kyobo.com/dgt/web/dtp/insurance-detail?isPdtCd=1000881&evcode=DCJ_MO',
-            name: '(무)교보내맘쏙건강보험(DM)',
+            url: "https://mdirect.kyobo.co.kr:8877/biz/intro/introMain.do?wrkType=GM_EV_PM",
+            name: "내보험바로알기 서비스",
             imgUrl: '/resources/v1/img/img-ci-pd.png',
             explain: '부족한 보장을 꽉 채운 건강보험',
             certificationMsg: '생명보험협회 심의필 제 2022-05466호',
             period: "(2022-12-27 ~ 2023-12-26)"
         },
         gaParams: {
-            // 매개변수1 : [ep_button_area] 자기개발_요즘20대들은모른다는문제_상세
             params1: "질병_척추근골격계_버섯목증후군_상세",
-            // 매개변수2: [ep_button_area2] 상품페이지 이동
             params2: "상품페이지 이동",
-            // 매개변수3: [ep_button_name] 배너선택
             params3: "배너선택",
-            // 매개변수4: [ep_click_variable] "{{상품}}" 상품_(무)교보시작해요저축보험
-            params4: "상품_(무)교보내맘쏙건강보험(DM)",
+            params4: "DBS_상품_(무)교보내맘쏙건강보험(DM)",
         }
     },
     {
-        contentsId : "8B_110",
+        contentsId: "4B_043",
         linkInfoForInsurance: {
-            url: 'http://www.kyobo.com/dgt/web/dtp/insurance-detail?isPdtCd=1000710&evcode=DCJ_MO',
-            name: '(무)교보하나로케어종신보험Ⅱ(보증비용부과형)',
-            imgUrl: '/resources/v1/img/img-ci-pd.png',
-            explain: '평생 보장에 연금전환까지 하나로 OK!',
-            certificationMsg: '생명보험협회 심의필 제 2023-02863호',
-            period: "(2023-06-27 ~ 2024-06-26)"
-        },
-        gaParams: {
-            // 매개변수1 : [ep_button_area] 자기개발_요즘20대들은모른다는문제_상세
-            params1: "일상생활_부부_외벌이_상세",
-            // 매개변수2: [ep_button_area2] 상품페이지 이동
-            params2: "상품페이지 이동",
-            // 매개변수3: [ep_button_name] 배너선택
-            params3: "배너선택",
-            // 매개변수4: [ep_click_variable] "{{상품}}" 상품_(무)교보시작해요저축보험
-            params4: "상품_(무)교보하나로케어종신보험Ⅱ(보증비용부과형)",
-        }
-    },
-    {
-        contentsId : "7D_003",
-        linkInfoForInsurance: {
-            url: 'http://www.kyobo.com/dgt/web/dtp/insurance-detail?isPdtCd=1000898&evcode=DCJ_MO',
-            name: '(무)교보간편해요건강보험(DM)',
-            imgUrl: '/resources/v1/img/img-ci-pd.png',
-            explain: '꼭 필요한 보장만으로 보험가입 OK!',
-            certificationMsg: '생명보험협회 심의필 제 2023-02760호',
-            period: "(2023-06-27 ~ 2024-06-26)"
-        },
-        gaParams: {
-            // 매개변수1 : [ep_button_area] 자기개발_요즘20대들은모른다는문제_상세
-            params1: "건강관리_운동외_건강검진_상세",
-            // 매개변수2: [ep_button_area2] 상품페이지 이동
-            params2: "상품페이지 이동",
-            // 매개변수3: [ep_button_name] 배너선택
-            params3: "배너선택",
-            // 매개변수4: [ep_click_variable] "{{상품}}" 상품_(무)교보시작해요저축보험
-            params4: "상품_(무)교보간편해요건강보험(DM)",
-        }
-    }
-    /************************************ 기존 콘텐츠 ************************************/
-    ,
-    {
-        contentsId : "2B_061",
-        linkInfoForInsurance: {
-            url: 'https://www.kyobo.com/dgt/web/dtp/insurance-detail?isPdtCd=1000938&evcode=DCJ_MO',
-            name: '교보1년저축보험(무배당)[D]',
-            imgUrl: '/resources/v1/img/img-ci-pd.png',
-            explain: '소액으로 차근차근 오늘부터 시작해요!',
-            certificationMsg: '생명보험협회 심의필 제 2023-02901호',
-            period: "(2023-07-04 ~ 2024-07-03)"
-        },
-        gaParams: {
-            // 매개변수1 : [ep_button_area] 자기개발_요즘20대들은모른다는문제_상세
-            params1: "자산관리_재테크_스노우볼효과_상세",
-            // 매개변수2: [ep_button_area2] 상품페이지 이동
-            params2: "상품페이지 이동",
-            // 매개변수3: [ep_button_name] 배너선택
-            params3: "배너선택",
-            // 매개변수4: [ep_click_variable] "{{상품}}" 상품_(무)교보시작해요저축보험
-            params4: "상품_교보1년저축보험(무배당)[D]",
-        }
-    },
-    {
-        contentsId : "3C_033",
-        linkInfoForInsurance: {
-            url: 'http://www.kyobo.com/dgt/web/dtp/insurance-detail?isPdtCd=1000948&evcode=DCJ_MO',
-            name: '(무)교보금쪽같은내아이보험(갱신형)Ⅱ(DM)',
-            imgUrl: '/resources/v1/img/img-ci-pd.png',
-            explain: '내 아이를 위한 금쪽같은 어린이 보험',
-            certificationMsg: '생명보험협회 심의필 제 2022-05464호',
-            period: "(2022-12-27 ~ 2023-12-26)"
-        },
-        gaParams: {
-            // 매개변수1 : [ep_button_area] 자기개발_요즘20대들은모른다는문제_상세
-            params1: "자녀_아동_유치원_상세",
-            // 매개변수2: [ep_button_area2] 상품페이지 이동
-            params2: "상품페이지 이동",
-            // 매개변수3: [ep_button_name] 배너선택
-            params3: "배너선택",
-            // 매개변수4: [ep_click_variable] "{{상품}}" 상품_(무)교보시작해요저축보험
-            params4: "상품_(무)교보금쪽같은내아이보험(갱신형)Ⅱ(DM)",
-        }
-    },
-    {
-        contentsId : "6G_054",
-        linkInfoForInsurance: {
-            url: 'http://www.kyobo.com/dgt/web/dtp/insurance-detail?isPdtCd=1000948&evcode=DCJ_MO',
-            name: '(무)교보금쪽같은내아이보험(갱신형)Ⅱ(DM)',
-            imgUrl: '/resources/v1/img/img-ci-pd.png',
-            explain: '내 아이를 위한 금쪽같은 어린이 보험',
-            certificationMsg: '생명보험협회 심의필 제 2022-05464호',
-            period: "(2022-12-27 ~ 2023-12-26)"
-        },
-        gaParams: {
-            // 매개변수1 : [ep_button_area] 자기개발_요즘20대들은모른다는문제_상세
-            params1: "자녀_태아&신생아_임신검사_상세",
-            // 매개변수2: [ep_button_area2] 상품페이지 이동
-            params2: "상품페이지 이동",
-            // 매개변수3: [ep_button_name] 배너선택
-            params3: "배너선택",
-            // 매개변수4: [ep_click_variable] "{{상품}}" 상품_(무)교보시작해요저축보험
-            params4: "상품_(무)교보금쪽같은내아이보험(갱신형)Ⅱ(DM)",
-        }
-    },
-    {
-        contentsId : "13G_045",
-        linkInfoForInsurance: {
-            url: 'http://www.kyobo.com/dgt/web/dtp/insurance-detail?isPdtCd=1000881&evcode=DCJ_MO',
-            name: '(무)교보내맘쏙건강보험(DM)',
+            url: "https://www.kyobo.com/dgt/web/dtp/ei/insurance-detail/1000967&evcode=KB_CJ_EI",
+            name: "교보e생활습관케어보험(무배당)",
             imgUrl: '/resources/v1/img/img-ci-pd.png',
             explain: '부족한 보장을 꽉 채운 건강보험',
             certificationMsg: '생명보험협회 심의필 제 2022-05466호',
             period: "(2022-12-27 ~ 2023-12-26)"
         },
         gaParams: {
-            // 매개변수1 : [ep_button_area] 자기개발_요즘20대들은모른다는문제_상세
-            params1: "질병_당뇨_식단_상세",
-            // 매개변수2: [ep_button_area2] 상품페이지 이동
+            params1: "질병_척추근골격계_버섯목증후군_상세",
             params2: "상품페이지 이동",
-            // 매개변수3: [ep_button_name] 배너선택
             params3: "배너선택",
-            // 매개변수4: [ep_click_variable] "{{상품}}" 상품_(무)교보시작해요저축보험
-            params4: "상품_(무)교보내맘쏙건강보험(DM)",
+            params4: "DBS_상품_(무)교보내맘쏙건강보험(DM)",
         }
     },
     {
-        contentsId : "5B_041",
+        contentsId: "8B_110",
         linkInfoForInsurance: {
-            url: 'http://www.kyobo.com/dgt/web/dtp/insurance-detail?isPdtCd=1000881&evcode=DCJ_MO',
-            name: '(무)교보내맘쏙건강보험(DM)',
+            url: "https://mdirect.kyobo.co.kr:8877/biz/intro/introMain.do?wrkType=GM_EV_PM",
+            name: "내보험바로알기 서비스",
             imgUrl: '/resources/v1/img/img-ci-pd.png',
             explain: '부족한 보장을 꽉 채운 건강보험',
             certificationMsg: '생명보험협회 심의필 제 2022-05466호',
             period: "(2022-12-27 ~ 2023-12-26)"
         },
         gaParams: {
-            // 매개변수1 : [ep_button_area] 자기개발_요즘20대들은모른다는문제_상세
-            params1: "건강관리_운동_바디프로필_상세",
-            // 매개변수2: [ep_button_area2] 상품페이지 이동
+            params1: "질병_척추근골격계_버섯목증후군_상세",
             params2: "상품페이지 이동",
-            // 매개변수3: [ep_button_name] 배너선택
             params3: "배너선택",
-            // 매개변수4: [ep_click_variable] "{{상품}}" 상품_(무)교보시작해요저축보험
-            params4: "상품_(무)교보내맘쏙건강보험(DM)",
+            params4: "DBS_상품_(무)교보내맘쏙건강보험(DM)",
+        }
+    },
+    {
+        contentsId: "7D_003",
+        linkInfoForInsurance: {
+            url: "https://mdirect.kyobo.co.kr:8877/biz/intro/introMain.do?wrkType=GM_EV_PM",
+            name: "내보험바로알기 서비스",
+            imgUrl: '/resources/v1/img/img-ci-pd.png',
+            explain: '부족한 보장을 꽉 채운 건강보험',
+            certificationMsg: '생명보험협회 심의필 제 2022-05466호',
+            period: "(2022-12-27 ~ 2023-12-26)"
+        },
+        gaParams: {
+            params1: "질병_척추근골격계_버섯목증후군_상세",
+            params2: "상품페이지 이동",
+            params3: "배너선택",
+            params4: "DBS_상품_(무)교보내맘쏙건강보험(DM)",
+        }
+    },
+    {
+        contentsId: "9B_062",
+        linkInfoForInsurance: {
+            url: "https://www.kyobo.com/dgt/web/dtp/ei/insurance-detail/1000969&evcode=KB_CJ_EI",
+            name: "교보e감염케어보험(무배당)",
+            imgUrl: '/resources/v1/img/img-ci-pd.png',
+            explain: '부족한 보장을 꽉 채운 건강보험',
+            certificationMsg: '생명보험협회 심의필 제 2022-05466호',
+            period: "(2022-12-27 ~ 2023-12-26)"
+        },
+        gaParams: {
+            params1: "질병_척추근골격계_버섯목증후군_상세",
+            params2: "상품페이지 이동",
+            params3: "배너선택",
+            params4: "DBS_상품_(무)교보내맘쏙건강보험(DM)",
+        }
+    },
+    {
+        contentsId: "12G_108",
+        linkInfoForInsurance: {
+            url: "https://www.kyobo.com/dgt/web/dtp/ei/insurance-detail/1000969&evcode=KB_CJ_EI",
+            name: "교보e감염케어보험(무배당)",
+            imgUrl: '/resources/v1/img/img-ci-pd.png',
+            explain: '부족한 보장을 꽉 채운 건강보험',
+            certificationMsg: '생명보험협회 심의필 제 2022-05466호',
+            period: "(2022-12-27 ~ 2023-12-26)"
+        },
+        gaParams: {
+            params1: "질병_척추근골격계_버섯목증후군_상세",
+            params2: "상품페이지 이동",
+            params3: "배너선택",
+            params4: "DBS_상품_(무)교보내맘쏙건강보험(DM)",
+        }
+    },
+    {
+        contentsId: "14G_057",
+        linkInfoForInsurance: {
+            url: "https://mdirect.kyobo.co.kr:8877/biz/intro/introMain.do?wrkType=GM_EV_PM",
+            name: "내보험바로알기 서비스",
+            imgUrl: '/resources/v1/img/img-ci-pd.png',
+            explain: '부족한 보장을 꽉 채운 건강보험',
+            certificationMsg: '생명보험협회 심의필 제 2022-05466호',
+            period: "(2022-12-27 ~ 2023-12-26)"
+        },
+        gaParams: {
+            params1: "질병_척추근골격계_버섯목증후군_상세",
+            params2: "상품페이지 이동",
+            params3: "배너선택",
+            params4: "DBS_상품_(무)교보내맘쏙건강보험(DM)",
+        }
+    },
+    {
+        contentsId: "18B_023",
+        linkInfoForInsurance: {
+            url: "https://www.kyobo.com/dgt/web/dtp/ei/insurance-detail/1000978&evcode=KB_CJ_EI",
+            name: "교보e암케어보험(무배당,서비스서택형)",
+            imgUrl: '/resources/v1/img/img-ci-pd.png',
+            explain: '부족한 보장을 꽉 채운 건강보험',
+            certificationMsg: '생명보험협회 심의필 제 2022-05466호',
+            period: "(2022-12-27 ~ 2023-12-26)"
+        },
+        gaParams: {
+            params1: "질병_척추근골격계_버섯목증후군_상세",
+            params2: "상품페이지 이동",
+            params3: "배너선택",
+            params4: "DBS_상품_(무)교보내맘쏙건강보험(DM)",
+        }
+    },
+    {
+        contentsId: "19G_142",
+        linkInfoForInsurance: {
+            url: "https://www.kyobo.com/dgt/web/dtp/ei/insurance-detail/1000975&evcode=KB_CJ_EI",
+            name: "교보e뇌·심장케어보험(무배당,서비스선택형)",
+            imgUrl: '/resources/v1/img/img-ci-pd.png',
+            explain: '부족한 보장을 꽉 채운 건강보험',
+            certificationMsg: '생명보험협회 심의필 제 2022-05466호',
+            period: "(2022-12-27 ~ 2023-12-26)"
+        },
+        gaParams: {
+            params1: "질병_척추근골격계_버섯목증후군_상세",
+            params2: "상품페이지 이동",
+            params3: "배너선택",
+            params4: "DBS_상품_(무)교보내맘쏙건강보험(DM)",
         }
     }
 ]
